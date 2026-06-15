21dda21442875aece57dbc1feab2002648c689da
Alexander Gottweis <stablestud@protonmail.com>
2026-06-15 14:43:20 +0200

    Update XMLBlueprint

--- automatic/xmlblueprint/tools/chocolateyinstall.ps1
+++ automatic/xmlblueprint/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://filedn.eu/l6hrQdIONMfS36XFW6FwzhS/xmlblueprint-22.2026.06.02.exe'
-$checksum   = 'aa1e65559ca9403cb1e93045a56af5712751ffd74fbd83062ee51aa77050b7d5'
+$url        = 'https://filedn.eu/l6hrQdIONMfS36XFW6FwzhS/xmlblueprint-22.2026.06.11.exe'
+$checksum   = '3e99e1a5fd0bb389e3144e51afad9389ff7b9d163e053a9c6fd3fba7cba6aa5d'
 $installerArgs = $env:ChocolateyPackageParameters
 $packageArgs = @{
     packageName   = $env:ChocolateyPackageName
